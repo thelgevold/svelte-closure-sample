1d9db487dff4fd7e359679d7ff341738551c3785
Torgeir Helgevold <torgeirh@hotmail.com>
2020-08-30 10:06:17 -0400

    Add compression

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const compression = require("compression");
 
 const app = express();
+
+app.use(compression({ level: 1 }));
 
 app.use("/build", express.static("./public/build"));
 
