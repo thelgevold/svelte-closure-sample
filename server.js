9ed5309401c1e35ea93faec59a971e651d6f93dd
Torgeir Helgevold <torgeirh@hotmail.com>
2020-09-06 10:29:55 -0400

    Use Angular http from Svelte

--- server.js
+++ server.js
@@ -11,5 +11,17 @@ app.get("/", (_req, res) => {
   res.sendFile("index.html", { root: "./public" });
 });
 
+app.get("/angular-http1", (_req, res) => {
+  res.json({ greeting: 'hello-world1' });
+});
+
+app.get("/angular-http2", (_req, res) => {
+  res.json({ greeting: 'hello-world2' });
+});
+
+app.post("/angular-http-post", (_req, res) => {
+  res.json({ greeting: 'Hello from post' });
+});
+
 const port = process.env.PORT || 8001;
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
--- src/App.svelte
+++ src/App.svelte
@@ -1,116 +1,44 @@
 <script>
-  import Greeting from "./greeting/Greeting.svelte";
-  import Friends from "./friends/Friends.svelte";
-  import MergeSort from "./merge-sort/MergeSort.svelte";
-  import Treeview from "./treeview/Treeview.svelte";
-  import Grid from "./grid/Grid.svelte";
-  import Spredsheet from "./spreadsheet/Spreadsheet.svelte";
+  import {
+    HttpClient,
+    ɵHttpInterceptingHandler,
+    HttpXhrBackend,
+  } from "@angular/common/http";
 
-  import { Directory } from "./treeview/directory";
-  import { range, interval } from "rxjs";
   import { map, filter, tap, take } from "rxjs/operators";
+  import { forkJoin } from "rxjs";
 
-  import { GreetingService } from "./services/greeting-service";
-  import { StreamService } from "./services/stream-service";
-
-  export let title;
-
-  let greetingText = "";
-  let visible = true;
-
-  const greetingService = new GreetingService();
-  const streamService = new StreamService();
-
-  let greetings = greetingService.getGreetings();
-
-  function addGreeting() {
-    const newGreeting = greetingService.createGreeting(greetingText);
-    greetings = [...greetings, { greetingMsg: greetingText }];
-  }
-
-  range(1, 200)
-    .pipe(
-      filter((x) => x % 2 === 1),
-      map((x) => x + x)
-    )
-    .subscribe((x) => console.log(`Value: ${x}`));
-
-  const int = interval(1000).pipe(
-    map((v) => v * 100),
-    tap((v) => console.log(v)),
-    take(10)
-  );
-  int.subscribe();
-
-  const fall2014 = new Directory("Fall 2014", []);
-  const summer2014 = new Directory("Summer 2014", []);
-
-  const pics = new Directory("Pictures", [summer2014, fall2014]);
-
-  const music = new Directory("Music", []);
-
-  let directories = [new Directory("Directories", [pics, music])];
-</script>
-
-<style>
-  main {
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  ul {
-    list-style-type: none;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
+  class BrowserXhr {
+    build() {
+      return new XMLHttpRequest();
     }
   }
-</style>
+
+  var handler = new ɵHttpInterceptingHandler(
+    new HttpXhrBackend(new BrowserXhr()),
+    {
+      get: (a) => [],
+    }
+  );
+  const httpClient = new HttpClient(handler);
+
+  let httpResult = { msg1: "", msg2: "", msg3: "" };
+
+  forkJoin(
+    httpClient.get("/angular-http1"),
+    httpClient.get("/angular-http2"),
+    httpClient.post("/angular-http-post")
+  ).subscribe((res) => {
+    httpResult.msg1 = res[0]["greeting"];
+    httpResult.msg2 = res[1]["greeting"];
+    httpResult.msg3 = res[2]["greeting"];
+  });
+</script>
 
 <main>
-  <h1>{title}!</h1>
+  <h1>Angular Http</h1>
 
-  <Spredsheet />
-
-  <Grid />
-
-  <Treeview {directories} />
-
-  <Friends />
-
-  <MergeSort />
-
-  {#if greetings.length > 10}
-    <p>More than 10 greetings</p>
-  {:else}
-    <p>Less than 10 greetings</p>
-  {/if}
-
-  <ul>
-    {#each greetings as greeting}
-      <Greeting {greeting} />
-    {/each}
-  </ul>
-
-  <button on:click={addGreeting}>Add Greeting</button>
-  <input bind:value={greetingText} />
-
-  <br />
-
-  <div>
-    <button on:click={streamService.mergeStreams}>Merge Streams</button>
-    <button on:click={streamService.concatStreams}>Concat Streams</button>
-    <button on:click={streamService.forkJoinStreams}>ForkJoin Streams</button>
-    <button on:click={streamService.flatMapStreams}>FlatMap Streams</button>
-  </div>
+  <h3>{httpResult.msg1}</h3>
+  <h3>{httpResult.msg2}</h3>
+  <h3>{httpResult.msg3}</h3>
 </main>
